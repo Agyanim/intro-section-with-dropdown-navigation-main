6d795bdf43e9f0d4ff7c444b7f12a8caf60d2e9e
Agyanim <71318355+Agyanim@users.noreply.github.com>
2022-10-06 16:16:45 +0000

    close menu added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+const featureList = document.getElementById("features");
+const company = document.getElementById("company");
+const subList1 = document.querySelector(".sub-list-1");
+const subList2 = document.querySelector(".sub-list-2");
 
+
+const displayFeatures =()=>{
+    featureList.addEventListener("click",()=>{
+        subList1.classList.toggle("toggle-1");
+        subList2.classList.remove("toggle-2");
+    })
+}
+const displayCompany=()=>{
+    company.addEventListener("click",()=>{
+        subList2.classList.toggle("toggle-2")
+        subList1.classList.remove("toggle-1");
+    })
+}
+// displayFeatures();
+// displayCompany();
